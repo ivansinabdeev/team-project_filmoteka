04942d5933e8c9ce4a0ebb05ea76b18edabaef4d
TommoBosha <116563699+TommoBosha@users.noreply.github.com>
2023-02-02 09:28:10 +0200

    add ref

---
+++ src/js/constants/ref.js
@@ -0,0 +1,14 @@
+export function refs() {
+    return {
+        searchRef: {
+            searchForm: document.querySelector('#searchForm'),
+        },
+
+        libraryButtonsRef: {
+            btnWatched: document.querySelector('.js-btn-watched'),
+            btnQueue: document.querySelector('.js-btn-queue'),
+        },
+
+
+    }
+}
