1fc96abc2b9f88f0ee9651c34a49a937df6a6991
TommoBosha <116563699+TommoBosha@users.noreply.github.com>
2023-02-04 00:52:18 +0200

    fix api

---
+++ src/js/apiService.js
@@ -0,0 +1,67 @@
+import axios from 'axios';
+
+export default class ApiService {
+    constructor(opt) {
+        this.searchQuery = opt.searchQuery;
+        this.page = opt.page;
+        this.key = 'api_key=ddec42efa7dad57904cab0b807fa902e';
+        this.genres = [];
+
+        this.fetchGenres();
+    }
+    findGenresById(ids) {
+        const arr = ids.flatMap(id => this.genres.filter(item => item.id === id));
+        const movieGenres = arr.map(el => el.name);
+        if (movieGenres.length === 0) {
+            return ['Genres not yet filled'];
+        }
+        return movieGenres.join(', ');
+    }
+    fetchGenres() {
+        axios
+            .get(
+                `https://api.themoviedb.org/3/genre/movie/list?${this.key}&language=en-US`
+            )
+            .then(res => {
+                this.genres = res.data.genres;
+            })
+            .catch(error => console.log(error));
+    }
+
+    async fetchMovies() {
+        sessionStorage.setItem('searchQuery', this.searchQuery);
+        sessionStorage.setItem('page', this.page);
+
+        if (this.searchQuery === '') {
+            return await this.fetchTrendMovies();
+        } else {
+            return await this.fetchByKeyWord();
+        }
+    }
+
+    // трендовые фильмы //
+    async fetchTrendMovies() {
+        try {
+            const url = `https://api.themoviedb.org/3/trending/movie/day?${this.key}&page=${this.page}`;
+            const response = await axios.get(url);
+            return response.data;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    //поиск по ключевому слову //
+    async fetchByKeyWord() {
+        try {
+            const url = `https://api.themoviedb.org/3/search/movie?${this.key}&language=en-US&query=${this.searchQuery}&page=${this.page}&include_adult=false`;
+            const response = await axios.get(url);
+            return response.data;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
+
+export const fetchApi = new ApiService({
+    page: Number(localStorage.getItem('page') || 1),
+    searchQuery: localStorage.getItem('searchQuery') || '',
+});
---
+++ src/js/pagin.js
@@ -0,0 +1,50 @@
+import Pagination from 'tui-pagination';
+import { fetchApi } from './apiService';
+import { markupFilm } from './card-markup';
+
+const container = document.getElementById('tui-pagination-container');
+
+const options = {
+    totalItems: 20000,
+    itemsPerPage: 20,
+    visiblePages: 5,
+    centerAlign: true,
+    page: Number(localStorage.getItem('page') || 1),
+    firstItemClassName: 'tui-first-child',
+    lastItemClassName: 'tui-last-child',
+};
+export let pagination;
+if (container) {
+    pagination = new Pagination(container, options);
+
+    pagination.on('afterMove', event => {
+        const galleryEl = document.querySelector('.movieList');
+        galleryEl.innerHTML = '';
+        fetchApi.page = event.page;
+        fetchApi.fetchMovies().then(handleSucces).catch(handleError);
+    });
+
+    fetchApi.fetchMovies().then(handleSucces).catch(handleError);
+}
+
+function handleSucces(data) {
+    const movies = data.results;
+    markupFilm(movies);
+}
+
+function handleError(error) {
+    console.error(error);
+}
+
+export function resetPagination() {
+    pagination.reset();
+}
+
+const currentPage = document.querySelector('.tui-pagination');
+if (currentPage) currentPage.addEventListener('click', onPageClick);
+
+function onPageClick(event) {
+    const page = event.target.textContent;
+    localStorage.setItem('page', page);
+    options.page = event.target.textContent;
+}
