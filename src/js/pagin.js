71655fb545c22f86696050d397f8abb07e3167e9
TommoBosha <116563699+TommoBosha@users.noreply.github.com>
2023-02-04 21:29:09 +0200

    fix bug

--- src/js/pagin.js
+++ src/js/pagin.js
@@ -286,7 +286,7 @@ const options = {
     lastItemClassName: 'tui-last-child',
 };
 
-let pagination;
+export let pagination;
 if (container) {
     pagination = new Pagination(container, options);
 
