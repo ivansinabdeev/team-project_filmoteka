d71b4669597e45e29bcc6568fe424d2a5ae3db3b
Vladyslav Marushchak <marushchakvl@icloud.com>
2023-02-05 12:57:35 +0200

    update cardFilm.js

--- src/js/cardFilm.js
+++ src/js/cardFilm.js
@@ -1,19 +1,18 @@
 import { playSpinner, stopSpinner } from "./spinner";
 
 function cardFilm() {   
-  return `
- <img src="poster path" alt="movie-poster" />
- <h2>title</h2>
- <div>
-   <p>Vote / Votes vote/votes</p>
-   <p>Popularity popularity</p>
-   <p>Original Title original title </p>
-   <p>Genre   genre</p>
- </div>
- <h3>About </h3>
- <p>overview</p>
- <button type="button">add to Watched</button>
- <button type="button">add to queue</button>
-` 
-}
-// new comment
+   return `
+   <img class = 'modal-img' src="${BASE_URL}${movie.poster_path}" alt="movie-poster" /></a>
+   <h2 class = 'modal-title'>${movie.title}</h2>
+   <div>
+      <p>Vote / Votes ${movie.vote_average}/${movie.vote_count}</p>
+      <p>Popularity ${movie.popularity}</p>
+      <p>Original Title ${movie.original_title} </p>
+      <p>Genre ${movie.genres}</p>
+   </div>
+   <h3>About </h3>
+   <p>${movie.overview}</p>
+   <button class="watched" type="button">add to Watched</button>
+   <button class="queue" type="button">add to queue</button>
+ `;
+ }
